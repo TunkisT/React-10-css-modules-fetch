44afac56bc7434ee08b71558a5287ff0c704f5f2
Tautvydas Tunkis <ttunkis@gmail.com>
2022-04-06 11:20:53 +0300

    cards

--- src/components/IconCard/IconCard.jsx
+++ src/components/IconCard/IconCard.jsx
@@ -1,5 +1,11 @@
+import css from './IconCard.module.css'
+
 const IconCard = () => {
-  return <div>Icon card</div>;
+  return <div className={css.cardDiv}>
+    <i className="fa fa-car fa-5x iconColor" ></i>
+    <h1 className={css.hOne}>Consequat lorem</h1>
+    <p className={css.para}>Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore.</p>
+    </div>;
 };
 
 export default IconCard;
