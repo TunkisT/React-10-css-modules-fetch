80e521ab4f639731d29fca7945fb1e16566f6176
Tautvydas Tunkis <ttunkis@gmail.com>
2022-04-06 11:51:12 +0300

    cards done

--- src/components/IconCard/IconCard.jsx
+++ src/components/IconCard/IconCard.jsx
@@ -1,11 +1,13 @@
-import css from './IconCard.module.css'
+import css from './IconCard.module.css';
 
-const IconCard = () => {
-  return <div className={css.cardDiv}>
-    <i className="fa fa-car fa-5x iconColor" ></i>
-    <h1 className={css.hOne}>Consequat lorem</h1>
-    <p className={css.para}>Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore.</p>
-    </div>;
+const IconCard = (props) => {
+  return (
+    <div className={css.cardDiv}>
+      <i className={`fa ${props.img} fa-5x iconColor`}></i>
+      <h1 className={css.hOne}>{props.title}</h1>
+      <p className={css.para}>{props.text}</p>
+    </div>
+  );
 };
 
 export default IconCard;
--- src/components/IconCardSection/IconCardSection.jsx
+++ src/components/IconCardSection/IconCardSection.jsx
@@ -1,18 +1,45 @@
 import IconCard from '../IconCard/IconCard';
 import css from './IconCardSection.module.css';
 
+const cardData = [
+  {
+    img: 'fa-eye',
+    title: 'Sinum melitamus',
+    text: 'Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore.',
+  },
+  {
+    img: 'fa-money',
+    title: 'Leram tasimotus',
+    text: 'Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore.',
+  },
+  {
+    img: 'fa-smile-o',
+    title: 'Kitarus o pelates',
+    text: 'Ornare nulla proin odio consequat sapien vestibulum ipsum primis sed amet consequat lorem dolore.',
+  },
+];
+
 const IconCardSection = () => {
   return (
     <section className={css.section}>
       <div className='container'>
         <h2 className={css.title}>Here's all the stuff I do.</h2>
-        <p className={css.para}>Odio turpis amet sed consequat eget posuere consequat.</p>
+        <p className={css.para}>
+          Odio turpis amet sed consequat eget posuere consequat.
+        </p>
         <div className='icon-cards grid'>
-          <IconCard />
-          <IconCard />
-          <IconCard />
+          {cardData.map((obj) => (
+            <IconCard
+              key={obj.img}
+              img={obj.img}
+              title={obj.title}
+              text={obj.text}
+            />
+          ))}
         </div>
-        <p className={css.para}>Lorem ipsum dolor sit sapien vestibulum ipsum primis?</p>
+        <p className={css.para}>
+          Lorem ipsum dolor sit sapien vestibulum ipsum primis?
+        </p>
         <button className={css.btn}>See some of my recent work</button>
       </div>
     </section>
